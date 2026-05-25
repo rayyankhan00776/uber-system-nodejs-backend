3445ea20bef4640032e28efe65e573345bd8e11e
rayyankhan00776 <rkkhan0750@gmail.com>
2026-05-25 19:46:47 +0500

    added the main gateway and connected the user part of it

---
+++ gateway/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { consoleLogger, fileLogger } from './middleware/morgan.middleware.js';
+
+import expressProxy from 'express-http-proxy';
+
+const app = express();
+
+app.use(consoleLogger);
+app.use(fileLogger);
+app.use('/v1/api/user', expressProxy('http://localhost:3001'));
+
+app.listen(3000, () => {
+    console.log('API Gateway is running on port 3000 🟢');
+})
